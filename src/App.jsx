1e9a2b53c107cc20759ba7e0c9e05689a93f7d7d
Gustavo Oliveira Garcia Ribeiro <gustavo.ribeiro@expocaccer.com.br>
2022-06-28 16:49:02 -0300

    Feat: Inicializando projeto.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header"
+import { Post } from "./Post"
+
+export function App() {
+
+  return (
+    <div>
+      
+      <Header />
+      
+    </div>
+  )
+}
+
