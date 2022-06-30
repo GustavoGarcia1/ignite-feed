7119285fdf88d7985182216868839e5e71f4c24a
Gustavo Oliveira Garcia Ribeiro <gustavo.ribeiro@expocaccer.com.br>
2022-06-30 09:56:17 -0300

    Feat: Criando component Post.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
 
 import './global.css'
 import styles from './App.module.css'
-import { Sidebar } from "./components/Sidebar"
 
 export function App() {
 
@@ -15,14 +15,7 @@ export function App() {
         <Sidebar />
         
         <main>
-          <Post 
-            author="Gustavo Garcia"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          />
-          <Post 
-            author="Ronaldo Nazário"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          />
+          <Post />
         </main>
       </div>
 
