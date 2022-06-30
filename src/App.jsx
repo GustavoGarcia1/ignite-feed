51b002d5319154820d4a86f0765b88944efc5f61
Gustavo Oliveira Garcia Ribeiro <gustavo.ribeiro@expocaccer.com.br>
2022-06-30 08:45:57 -0300

    Feat: Criando o componente Sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
+
 export function App() {
 
   return (
     <div>
-      
       <Header />
       
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+          <Post 
+            author="Gustavo Garcia"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+          <Post 
+            author="Ronaldo Nazário"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
