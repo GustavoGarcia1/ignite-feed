75b8ae16748fa72b55259f9a2e50a888dffb1f6b
Gustavo Oliveira Garcia Ribeiro <gustavo.ribeiro@expocaccer.com.br>
2022-07-07 17:17:50 -0300

    Feat: Adicionando validações o formulário.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,6 +31,7 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
   }
 
@@ -42,6 +43,12 @@ export function Post({ author, publishedAt, content }) {
     setComments(commentsWithoutDeletedOne)
   
   }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Este campo é obrigatório');
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -78,10 +85,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder='Deixe um comentário'
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
