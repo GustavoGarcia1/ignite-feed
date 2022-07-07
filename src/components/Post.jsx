ecd11248a7a72247c150d48c76bd7a5c0f864152
Gustavo Oliveira Garcia Ribeiro <gustavo.ribeiro@expocaccer.com.br>
2022-07-07 16:39:15 -0300

    Feat: Criando comunicação entre os componentes para deletar comentários.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,6 +34,15 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete
+    })
+
+    setComments(commentsWithoutDeletedOne)
+  
+  }
+
   return (
     <article className={styles.post}>
 
@@ -80,7 +89,9 @@ export function Post({ author, publishedAt, content }) {
         {comments.map(comment => {
           return (
             <Comment 
+              key={comment}
               content={comment}
+              onDeleteComment={deleteComment}
             />
           )
         })}
